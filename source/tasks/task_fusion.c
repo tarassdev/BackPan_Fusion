f56fb1d5ba8621135a3eaf4d19c6a564d19d1421
tarassdev <Mykolalysochenko@gmail.com>
2025-10-13 19:31:36 +0300

    task modification

--- source/tasks/task_fusion.c
+++ source/tasks/task_fusion.c
@@ -12,12 +12,9 @@
 #include "drivers/flexcan1_drv.h"
 //#include "fusion/Fusion.h"
 
-//static FusionAhrs ahrs;
-//static FusionOffset offset;
+
 
 void TaskFusion_Init(void) {
-//    FusionAhrsInitialise(&ahrs);
-//    FusionOffsetInitialise(&offset, 200); // sample rate 200 Hz (5 ms)
 
     xTaskCreate(TaskFusion, "Fusion_Task", 1024, NULL, tskIDLE_PRIORITY + 4, NULL);
 }
@@ -26,36 +23,6 @@ void TaskFusion_Init(void) {
  * @brief Runs Fusion algorithm every 5 ms triggered by PIT ISR.
  */
 void TaskFusion(void *pvParameters) {
-//    FusionVector gyro, acc, mag;
-//    FusionEuler euler;
-//
-//    for (;;) {
-//        // Wait for periodic trigger (5 ms)
-//        xSemaphoreTake(xSemFusionTick, portMAX_DELAY);
-//
-//        // Get latest IMU sample
-//        xSemaphoreTake(xMutexIMUData, portMAX_DELAY);
-//        IMU_Data_t imu = g_imuData;
-//        xSemaphoreGive(xMutexIMUData);
-//
-//        gyro = imu.gyro;
-//        acc  = imu.acc;
-//        mag  = imu.mag;
-//
-//        // Update offset + AHRS
-//        gyro = FusionOffsetUpdate(&offset, gyro);
-//        FusionAhrsUpdate(&ahrs, gyro, acc, mag, 0.005f); // 5 ms
-//
-//        euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
-//
-//        // Save result
-//        xSemaphoreTake(xMutexFusionData, portMAX_DELAY);
-//        g_fusionOutput.euler = euler;
-//        xSemaphoreGive(xMutexFusionData);
-//
-//        // Notify CANopen task to send
-//        xSemaphoreGive(xSemFusionResultReady);
-//    }
 
 	for (;;) {
 
